1ba54fb2e1e06a610f0f84a00d6974adf5ac4931
twilightscapes <ihatespam@gmail.com>
2019-12-06 14:31:03 -0600

    nav smear fix

--- src/pages/galleries/gallery1.js
+++ src/pages/galleries/gallery1.js
@@ -19,6 +19,15 @@ background: -webkit-linear-gradient(left,  #9b0c92 0%,#e812e5 39%,#db00c9 62%,#9
 background: linear-gradient(to right,  #9b0c92 0%,#e812e5 39%,#db00c9 62%,#9b0c92 100%);
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#9b0c92', endColorstr='#9b0c92',GradientType=1 );
 }
+#galleries:before{
+content:" ";
+position:absolute;
+top:0;
+left:0;
+width:100%;
+height:100%;
+background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%2319181a' fill-opacity='0.15' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
+}
 
 
 .intro:before{
